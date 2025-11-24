06db6ce7845bbc1eaf35d4932bdd30d9f43c4b86
ly <nick3108@qq.com>
2025-11-24 11:36:30 +0800

    feat: video skeleton

--- src/modules/videos/ui/sections/video-section.tsx
+++ src/modules/videos/ui/sections/video-section.tsx
@@ -4,9 +4,9 @@ import { cn } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { VideoPlayer } from "../components/video-player";
+import { VideoPlayer, VideoPlayerSkeleton } from "../components/video-player";
 import { VideoBanner } from "../components/video-banner";
-import { VideoTopRow } from "../components/video-top-row";
+import { VideoTopRow, VideoTopRowSkeleton } from "../components/video-top-row";
 import { useAuth } from "@clerk/nextjs";
 
 interface VideoSectionProps {
@@ -24,7 +24,12 @@ export const VideoSection = ({ videoId }: VideoSectionProps) => {
 };
 
 const VideoSectionSkeleton = () => {
-  return <div>Loading...</div>;
+  return (
+    <>
+      <VideoPlayerSkeleton />
+      <VideoTopRowSkeleton />
+    </>
+  );
 };
 
 const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
